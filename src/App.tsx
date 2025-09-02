5d9a02c0434ef85f7efe05341830fb193800f224
Sapon Wongkrasaemongkol <Sapon2548@gmail.com>
2025-09-02 11:24:17 +0700

    feat: 🎸 Change Structure and Add Dockerfile
    Change default port to 3000

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import { Card,CardContent,CardDescription,CardFooter,CardTitle,CardHeader} from "./outbound/ui/components/mock/card"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@client/ui/components/mock/card";
+
 export default function App() {
-  return ( 
+  return (
     <>
- <Card className="w-1/5">
-      <CardHeader>
-        <CardTitle>Title</CardTitle>
-        <CardDescription>Description</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>Main</p>
-      </CardContent>
-      <CardFooter>
-        <button className="px-3 py-1 bg-blue-500 text-white rounded">
-         Some button
-        </button>    
-      </CardFooter>
-    </Card>
+      <Card className="w-1/5">
+        <CardHeader>
+          <CardTitle>Title</CardTitle>
+          <CardDescription>Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Main</p>
+        </CardContent>
+        <CardFooter>
+          <button className="px-3 py-1 bg-blue-500 text-white rounded">
+            Some button
+          </button>
+        </CardFooter>
+      </Card>
     </>
-  )
+  );
 }
