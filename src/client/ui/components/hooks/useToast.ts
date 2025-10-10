250154acffadde1215929834a23cd81d289a3ec0
Peanut <Sapon2548@gmail.com>
2025-10-11 06:52:29 +0700

    fix: 🐛 Fix Var

--- src/client/ui/components/hooks/useToast.ts
+++ src/client/ui/components/hooks/useToast.ts
@@ -12,8 +12,7 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const actionTypes = {
+const _actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
   DISMISS_TOAST: "DISMISS_TOAST",
@@ -27,7 +26,7 @@ function genId() {
   return count.toString()
 }
 
-type ActionType = typeof actionTypes
+type ActionType = typeof _actionTypes
 
 type Action =
   | {
