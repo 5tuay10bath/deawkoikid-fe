af7516f17d8833971dab57bd6fb8869c936ada61
Iamyasuomain <johncekas20@gmail.com>
2025-10-18 04:51:53 +0700

    feat: 🎸 Update Dockerfile and entrypoint for dynamic API URL configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# Replace placeholder with runtime env
-sed -i "s|REPLACE_API_URL|${DEAWKOIKID_API_BASE_URL}|g" /usr/share/nginx/html/env.js
-
-# Start Nginx
-exec "$@"
+set -e
+sed -i "s|REPLACE_API_URL|${DEAWKOIKID_API_BASE_URL:-http://localhost:8080}|g" /app/site/config.js
+exec serve -s site -l tcp://0.0.0.0:3000
